6c0646fdc0ee589194339f5daeced2e05e5cac35
Sadnan-2019 <sadnan.wow@gmail.com>
2022-11-15 15:57:23 +0600

    gon on

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,6 +1,45 @@
+import { Chart } from "chart.js";
 import React from "react";
 
 const DashBoard = () => {
+     
+    document.addEventListener("DOMContentLoaded", () => {
+      new Chart (document.querySelector('#lineChart'), {
+        type: 'line',
+        data: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [{
+            label: 'Line Chart',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    });
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div>
       <main id="main" class="main">
@@ -210,7 +249,22 @@ const DashBoard = () => {
                         Reports <span>/Today</span>
                       </h5>
 
-                      <div id="reportsChart"></div>
+                      {/* <div id="reportsChart">
+
+                        <h2> One Chart is here</h2>
+                      </div> */}
+                      <div class="col-lg-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Line Chart</h5>
+
+          
+              <canvas id="lineChart"  style={{maxHeight:"800px"}}></canvas>
+              
+
+            </div>
+          </div>
+        </div>
                     </div>
                   </div>
                 </div>
@@ -597,12 +651,28 @@ const DashBoard = () => {
                     Budget Report <span>| This Month</span>
                   </h5>
 
-                  <div
+                  {/* <div
                     id="budgetChart"
                     // style="min-height: 400px;"
                     style={{minHeight:"400px"}}
-                    class="echart"
-                  ></div>
+                     
+                  ></div> */}
+
+
+
+<div class="col-lg-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Line Chart</h5>
+
+            
+              <canvas id="lineChart"  ></canvas>
+            
+             
+
+            </div>
+          </div>
+        </div>
                 </div>
               </div>
 
