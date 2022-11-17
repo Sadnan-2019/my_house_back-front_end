d7f5bac79116b24800d166da775cfbfa4d5418b6
Sadnan-2019 <sadnan.wow@gmail.com>
2022-11-17 12:24:33 +0600

    gon on

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,13 +7,13 @@ const Footer = () => {
         <div class="copyright">
           &copy; Copyright{" "}
           <strong>
-            <span>NiceAdmin</span>
+            <span>My House</span>
           </strong>
           . All Rights Reserved
         </div>
-        <div class="credits">
-          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-        </div>
+        {/* <div class="credits">
+          Designed by <a href="https://bootstrapmade.com/"></a>
+        </div> */}
       </footer>
 
      
